6519aec9dbcfbece3403e3f765b912c2b44e0554
devtanc <devtanc@gmail.com>
2019-09-20 23:43:01 -0600

    add nestest rom and main loading logic
    - updates to debugging terminal ui

--- main.rs
+++ main.rs
@@ -7,11 +7,15 @@ mod data_flow;
 mod event;
 
 use bus::Bus;
+use cpu::instruction::{get_instruction, Instruction};
 use cpu::Cpu;
-use data_flow::{HexByte, MemoryAddress, ReadRange, ReadWrite};
+use data_flow::{HexByte, HexSlice, MemoryAddress, ReadRange, ReadWrite};
 use event::{Config, Event, Events};
 
+use std::env;
+use std::fs::File;
 use std::io;
+use std::io::prelude::*;
 use std::time::Duration;
 
 use termion::event::Key;
@@ -23,11 +27,15 @@ use tui::style::{Color, Modifier, Style};
 use tui::widgets::{Block, Borders, Paragraph, Text, Widget};
 use tui::{Frame, Terminal};
 
-const PROGRAM_START_ADDR: u16 = 0x8000;
+const CARTRIDGE_ADDR: u16 = 0x4020;
 const ZERO_PAGE_START: u16 = 0x0000;
+const STACK_BASE_ADDR: u16 = 0x0100;
 const DEFAULT_TICK_RATE: u64 = 200;
+const BYTES_PER_ROW: u16 = 16;
 
 fn main() -> Result<(), failure::Error> {
+  let args: Vec<String> = env::args().collect();
+  let filename = &args[1];
   let events = Events::with_config(Config {
     tick_rate: Duration::from_millis(DEFAULT_TICK_RATE),
     ..Config::default()
@@ -38,13 +46,16 @@ fn main() -> Result<(), failure::Error> {
 
   let mut cpu = Cpu::new();
 
-  load_program_memory(cpu.get_mut_bus_ref());
+  load_program_memory(cpu.get_mut_bus_ref(), filename)?;
 
   cpu.reset();
-
   println!("{}", termion::clear::All);
 
   loop {
+    if *cpu.get_current_tick() < 324 {
+      cpu.clock();
+      continue;
+    }
     draw_ui(&mut terminal, &mut cpu)?;
     match events.next()? {
       Event::Input(key) => match key {
@@ -62,9 +73,7 @@ fn main() -> Result<(), failure::Error> {
         }
         _ => {}
       },
-      Event::Tick => {
-        () // Do something here if you need to in between clocks
-      }
+      Event::Tick => (),
     }
   }
 
@@ -82,33 +91,49 @@ where
       .margin(1)
       .constraints([Constraint::Percentage(60), Constraint::Percentage(40)].as_ref())
       .split(f.size());
-    draw_memory_panel(&mut f, chunks[0], cpu.get_mut_bus_ref());
+    draw_memory_panel(&mut f, chunks[0], cpu);
     draw_instructions_panel(&mut f, chunks[1], cpu);
   })
 }
 
-fn load_program_memory(bus: &mut Bus) {
-  // Load program counter reset addresses with program start address
-  bus.write_addr(0xFFFC, ((PROGRAM_START_ADDR >> 8) & 0x00FF) as u8);
-  bus.write_addr(0xFFFD, (PROGRAM_START_ADDR & 0x00FF) as u8);
+fn load_program_memory(bus: &mut Bus, filename: &String) -> std::io::Result<()> {
+  let mut cartridge_memory = Vec::new();
 
-  // Load program into memory
-  const PROGRAM: &str =
-    "A2 0A 8E 00 00 A2 03 8E 01 00 AC 00 00 A9 00 18 6D 01 00 88 D0 FA 8D 02 00 EA EA EA";
-  let mut start = PROGRAM_START_ADDR;
-  let steps = PROGRAM.split(" ");
-  for step in steps {
-    match u8::from_str_radix(step, 16) {
-      Ok(data) => {
-        bus.write_addr(start, data);
-        start += 1;
+  let mut cartridge = File::open(filename)?;
+  cartridge.read_to_end(&mut cartridge_memory)?;
+
+  if filename == "nestest.nes" {
+    load_nestest(bus, &cartridge_memory);
+  } else {
+    let mut current_address = CARTRIDGE_ADDR;
+    for byte in cartridge_memory {
+      bus.write_addr(current_address, byte);
+      if current_address == 0xFFFF {
+        break;
       }
-      Err(_) => (),
+      current_address += 1;
     }
+  }
+
+  Ok(())
+}
+
+fn load_nestest(bus: &mut Bus, memory: &Vec<u8>) {
+  let mirrored_memory = &memory[0x0010..0x4010];
+  let mut current_address = 0x8000 - 1;
+
+  for byte in mirrored_memory {
+    bus.write_addr(current_address, *byte);
+    current_address += 1;
+  }
+  current_address = 0xC000 - 1;
+  for byte in mirrored_memory {
+    current_address += 1;
+    bus.write_addr(current_address, *byte);
   }
 }
 
-fn draw_instructions_panel<B>(f: &mut Frame<B>, area: Rect, cpu: &Cpu)
+fn draw_instructions_panel<B>(f: &mut Frame<B>, area: Rect, cpu: &mut Cpu)
 where
   B: Backend,
 {
@@ -117,10 +142,15 @@ where
     .constraints([Constraint::Percentage(30), Constraint::Percentage(70)].as_ref())
     .split(area);
   draw_status_register(f, chunks[0], cpu);
-  Block::default()
-    .title(" Instructions ")
-    .borders(Borders::ALL)
-    .render(f, chunks[1]);
+  draw_memory_page(
+    f,
+    chunks[1],
+    cpu,
+    0x0200,
+    32 * BYTES_PER_ROW,
+    " Results ",
+    0x0200,
+  );
 }
 
 fn draw_status_register<B>(f: &mut Frame<B>, area: Rect, cpu: &Cpu)
@@ -133,11 +163,11 @@ where
   for flag in &flags {
     match cpu.get_flag(*flag) {
       true => text.push(Text::styled(
-        format!("{}  ", flag),
+        format!("{}  ", expand_flag(*flag)),
         Style::default().modifier(Modifier::BOLD).fg(Color::Green),
       )),
       false => text.push(Text::styled(
-        format!("{}  ", flag),
+        format!("{}  ", expand_flag(*flag)),
         Style::default().modifier(Modifier::BOLD).fg(Color::Red),
       )),
     };
@@ -168,13 +198,19 @@ where
   let pc = MemoryAddress::new(*cpu.get_pc());
   text.push(Text::raw(format!("0x{}", pc)));
 
-  text.push(Text::raw("\nCurr Opcode:    "));
-  let opcode = HexByte::new(*cpu.get_opcode());
-  text.push(Text::raw(format!("0x{}", opcode)));
+  text.push(Text::raw("\nInstruc Addr:   "));
+  let ppc = MemoryAddress::new(*cpu.get_ppc());
+  text.push(Text::raw(format!("0x{}", ppc)));
 
-  text.push(Text::raw("\nData Addr:      "));
+  text.push(Text::raw("\nOperation:      "));
+  let opcode = *cpu.get_opcode();
+  let hex_opcode = HexByte::new(opcode);
+  let instruction = get_instruction(opcode);
   let data_addr = MemoryAddress::new(*cpu.get_addr_of_data());
-  text.push(Text::raw(format!("0x{}", data_addr)));
+  text.push(Text::raw(format!("0x{}  -> ", hex_opcode)));
+  text.push(Text::raw(format!("[{}]:", instruction.get_operation())));
+  text.push(Text::raw(format!("[{}]:", instruction.get_address_mode())));
+  text.push(Text::raw(format!("[{}]", data_addr)));
 
   text.push(Text::raw("\nOpcode Cycles:  "));
   let curr_command_cycles = cpu.get_cycles();
@@ -195,31 +231,144 @@ where
     .render(f, area);
 }
 
-fn draw_memory_panel<B>(f: &mut Frame<B>, area: Rect, bus: &Bus)
+fn expand_flag(flag: char) -> String {
+  match flag {
+    'N' => "NEG".to_string(),
+    'V' => "OVR".to_string(),
+    'U' => "-U-".to_string(),
+    'B' => "BRK".to_string(),
+    'D' => "DEC".to_string(),
+    'I' => "INT".to_string(),
+    'Z' => "ZR0".to_string(),
+    'C' => "CAR".to_string(),
+    _ => "".to_string(),
+  }
+}
+
+fn draw_memory_panel<B>(f: &mut Frame<B>, area: Rect, cpu: &mut Cpu)
 where
   B: Backend,
 {
   let chunks = Layout::default()
     .direction(Direction::Vertical)
-    .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
+    .constraints(
+      [
+        Constraint::Percentage(32),
+        Constraint::Percentage(32),
+        Constraint::Percentage(32),
+        Constraint::Percentage(6),
+      ]
+      .as_ref(),
+    )
     .split(area);
-  draw_memory_page(f, chunks[0], bus, ZERO_PAGE_START, " Zero Page ");
-  draw_memory_page(f, chunks[1], bus, PROGRAM_START_ADDR, " Program Memory ");
+  draw_memory_page(
+    f,
+    chunks[0],
+    cpu,
+    ZERO_PAGE_START,
+    18 * BYTES_PER_ROW,
+    " Zero Page ",
+    *cpu.get_addr_of_data(),
+  );
+  draw_memory_page(
+    f,
+    chunks[1],
+    cpu,
+    STACK_BASE_ADDR,
+    15 * BYTES_PER_ROW,
+    " Stack ",
+    *cpu.get_stkp() as u16 + STACK_BASE_ADDR,
+  );
+  draw_memory_page(
+    f,
+    chunks[2],
+    cpu,
+    *cpu.get_pc(),
+    18 * BYTES_PER_ROW,
+    " Program Memory ",
+    *cpu.get_pc(),
+  );
+  draw_memory_page(
+    f,
+    chunks[3],
+    cpu,
+    0xFFF0,
+    1 * BYTES_PER_ROW,
+    " Interrupt Vectors ",
+    *cpu.get_addr_of_data(),
+  );
 }
 
-fn draw_memory_page<B>(f: &mut Frame<B>, area: Rect, bus: &Bus, starting_address: u16, title: &str)
-where
+fn draw_memory_page<B>(
+  f: &mut Frame<B>,
+  area: Rect,
+  cpu: &mut Cpu,
+  address: u16,
+  offset: u16,
+  title: &str,
+  highlight_address: u16,
+) where
   B: Backend,
 {
+  let bus = cpu.get_mut_bus_ref();
   let mut text = vec![];
-  let ending_address = starting_address + 320;
+  let starting_address = match address {
+    0x0000..=0xFFF0 => address & 0xFFF0,
+    0xFFF1..=0xFFFF => 0xFFF0,
+  };
+  let ending_address = match (starting_address as u32) + (offset as u32) {
+    0x0000..=0xFFFF => starting_address + offset,
+    _ => 0xFFFF,
+  };
+
   for i in (starting_address..=ending_address).step_by(16) {
     let root_addr = MemoryAddress::new(i);
+    let offset_addr = i + 15;
+    let range = bus.read_range(i, offset_addr);
     text.push(Text::styled(
       format!("${}:   ", root_addr),
-      Style::default().modifier(Modifier::BOLD),
+      Style::default().modifier(Modifier::BOLD).fg(Color::Yellow),
     ));
-    text.push(Text::raw(format!("{}", bus.read_range(i, i + 16))));
+    if highlight_address != 0 && highlight_address >= i && highlight_address - i < 0x10 {
+      let highlight_offset = (highlight_address - i) as usize;
+      match highlight_offset {
+        0x00 => {
+          let highlight = &[range[0]];
+          let after = &range[1..];
+
+          text.push(Text::styled(
+            format!("{}", HexSlice::new(highlight)),
+            Style::default().bg(Color::DarkGray),
+          ));
+          text.push(Text::raw(format!("{}", HexSlice::new(after))));
+        }
+        0x01..=0xFE => {
+          let before = &range[..highlight_offset];
+          let highlight = &[range[highlight_offset]];
+          let after = &range[(highlight_offset + 1)..];
+
+          text.push(Text::raw(format!("{}", HexSlice::new(before))));
+          text.push(Text::styled(
+            format!("{}", HexSlice::new(highlight)),
+            Style::default().bg(Color::DarkGray),
+          ));
+          text.push(Text::raw(format!("{}", HexSlice::new(after))));
+        }
+        0xFF => {
+          let before = &range[..(highlight_offset - 1)];
+          let highlight = &[range[0xFF]];
+
+          text.push(Text::raw(format!("{}", HexSlice::new(before))));
+          text.push(Text::styled(
+            format!("{}", HexSlice::new(highlight)),
+            Style::default().bg(Color::DarkGray),
+          ));
+        }
+        _ => (),
+      };
+    } else {
+      text.push(Text::raw(format!("{}", HexSlice::new(range))));
+    }
     text.push(Text::raw("\n"));
   }
 
